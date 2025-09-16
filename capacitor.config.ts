d17db0da08354e008e158a37abc486a25953f620
Reyansh Raj Mishra <reyanshrajmishra8@gmail.com>
2025-09-16 13:37:40 +0530

    remove Android

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.shieldintelligence.shieldauth',
-  appName: 'SHIELD-Authenticator',
-  webDir: 'build'
-};
-
-export default config;

c21b3155fc2d72fc9805001f046192f08a4cefd7
Reyansh Raj Mishra <reyanshrajmishra8@gmail.com>
2025-09-16 12:45:50 +0530

    Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shieldintelligence.shieldauth',
+  appName: 'SHIELD-Authenticator',
+  webDir: 'build'
+};
+
+export default config;
